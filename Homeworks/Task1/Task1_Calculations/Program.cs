022275d3b0c197c197c2f816f59d2f5113927462
W1ngshot <kirill-samsonov@bk.ru>
2023-03-03 21:23:00 +0300

    refactoring

--- Homeworks/Task1/Task1_Calculations/Program.cs
+++ Homeworks/Task1/Task1_Calculations/Program.cs
@@ -1,28 +1,21 @@
-﻿using Task1_Calculations.Extensions;
+﻿using Task1_Calculations;
+using Task1_Calculations.Extensions;
 
-namespace Task1_Calculations;
-
-public static class Program
+var result = Calculator.CalculateResult();
+Console.WriteLine(result.GetAllProperties());
+        
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("Histogram values");
+var dict = result.Values.ToHistogram();
+foreach (var pair in dict)
 {
-    public static void Main(string[] args)
-    {
-        var result = Calculator.CalculateResult();
-        Console.WriteLine(result.GetAllProperties());
+    Console.WriteLine($"[{pair.Key} - {pair.Key + 1}): {pair.Value}%");
+}
         
-        Console.WriteLine("-------------------------------------------------");
-        Console.WriteLine("Значения для гистограммы");
-        var dict = result.Values.ToHistogram();
-        foreach (var pair in dict)
-        {
-            Console.WriteLine($"[{pair.Key} - {pair.Key + 1}): {pair.Value}%");
-        }
-        
-        Console.WriteLine("-------------------------------------------------");
-        Console.WriteLine("Значения для эмпирической функции распределения");
-        var function = result.Values.ToEmpiricalDistributionFunction();
-        foreach (var pair in function)
-        {
-            Console.WriteLine($"{pair.Key}: {pair.Value}");
-        }
-    }
+Console.WriteLine("-------------------------------------------------");
+Console.WriteLine("Empirical distribution function values");
+var function = result.Values.ToEmpiricalDistributionFunction();
+foreach (var pair in function)
+{
+    Console.WriteLine($"{pair.Key}: {pair.Value}");
 }
