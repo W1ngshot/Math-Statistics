8b39b1000f552256309abdfd6ec3a032bf696196
W1ngshot <kirill-samsonov@bk.ru>
2023-04-22 02:15:31 +0300

    Task5 & Task6 complete

---
+++ Homeworks/Task5/Task5_Calculations/Program.cs
@@ -0,0 +1,7 @@
+﻿var values = Parser.Parser.ParseXFromFile("../../../../Task5_Data.csv").ToList();
+
+Console.WriteLine(values.Sum(Math.Log));
+
+var alpha = (-values.Count / values.Sum(Math.Log) - 1) / 2;
+
+Console.WriteLine(alpha);
---
+++ Homeworks/Task6/Task6_Calculations/Program.cs
@@ -0,0 +1,15 @@
+﻿using MathNet.Numerics;
+using Statistics;
+
+const double q = 0.99d;
+var values = Parser.Parser.ParseXFromFile("../../../../Task6_Data.csv").ToList();
+var n = values.Count;
+var avg = Formulas.CalculateAverage(values);
+var deviation = Formulas.CalculateStandardDeviation(values);
+var m = deviation / Math.Sqrt(n - 1);
+var tCrit = ExcelFunctions.TInv(1 - q, n - 1);
+
+Console.WriteLine($"Объём выборки = {n}\n" +
+                  $"Выборочное среднее = {avg}\n" + 
+                  $"Стандартная ошибка среднего = {Math.Round(m, 2)}\n" + 
+                  $"Значения выборки с вероятностью {q} лежат в верхнем доверительном интервале (-[inf] ; {Math.Round(avg + tCrit * m, 2)}]");
